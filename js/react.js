07959359c6d9caf1f8bc25eb3d7e979d8ae3e181
Darrell Washington <darrellwjr.com@gmail.com>
2016-05-29 17:40:42 -0400

    React Composition
    Setting the Channels up.

--- js/react.js
+++ js/react.js
@@ -1,10 +1,37 @@
+
+let channels = [
+	{name: 'Hardware Support'},
+	{name: 'Software Support'}
+	
+];
+
 class Channel extends React.Component{
+	onClick() {
+		console.log('I was clicked', this.props.name);
+	}
 	render() {
 		return(
-			<li>Channel name</li>
+			<li onClick={this.onClick.bind(this)}>{this.props.name}</li>
 		)
 	}
 
 }
 
-ReactDOM.render(<Channel />, document.getElementById('app'));
+
+class ChannelList extends React.Component {
+	render(){
+		return(
+			<ul>
+				{this.props.channels.map( channel => {
+				 	return (
+				 		<Channel name= {channel.name} />
+				 		)
+					}
+				)}	
+			</ul>
+		)
+	}
+
+}
+
+ReactDOM.render(<ChannelList channels={channels} />, document.getElementById('app'));
